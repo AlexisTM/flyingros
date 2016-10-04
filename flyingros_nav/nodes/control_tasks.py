07c239461ae0080e20dae4dcad66d113542e5e5c
Alexis Paques <alexis.paques@gmail.com>
2016-10-04 11:10:10 +0200

    Catkinized

--- flyingros_nav/nodes/control_tasks.py
+++ flyingros_nav/nodes/control_tasks.py
@@ -4,20 +4,20 @@
 control_tasks.py
 
 This script sends positions to control the UAV in X, Y, Z using the
-tasks controller
+task controller
 
-ILPS is free software: you can redistribute it and/or modify
+FlyingROS is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-ILPS is distributed in the hope that it will be useful,
+FlyingROS is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with ILPS.  If not, see <http://www.gnu.org/licenses/>.
+along with FlyingROS.  If not, see <http://www.gnu.org/licenses/>.
 
 Software created by Alexis Paques and Nabil Nehri for the UCL
 in a Drone-Based Additive Manufacturing of Architectural Structures
