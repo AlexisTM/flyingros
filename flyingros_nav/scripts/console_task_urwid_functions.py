17848c66487ecde396b256128a5da32764f4a0a2
Alexis Paques <alexis.paques@gmail.com>
2016-10-06 04:38:17 +0530

    console.task

---
+++ flyingros_nav/scripts/console_task_urwid_functions.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+console_task_urwid_functions.py
+
+Thoses are urwid functions used in console_task.py. It allows to simplify the main script.
+
+This file is part of FlyingROS (Indoor Laser Positioning System).
+
+FlyingROS is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+FlyingROS is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with FlyingROS.  If not, see <http://www.gnu.org/licenses/>.
+
+Software created by Alexis Paques and Nabil Nehri for the UCL
+in a Drone-Based Additive Manufacturing of Architectural Structures
+project financed by the MIT Seed Fund
+
+Copyright (c) Alexis Paques 2016
+"""
