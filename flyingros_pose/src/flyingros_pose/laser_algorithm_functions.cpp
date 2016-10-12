3334988a2adf9d6c7b77d964635fbe3bba3fbcaf
Alexis Paques <alexis.paques@gmail.com>
2016-10-12 14:31:45 +0200

    laser functions

---
+++ flyingros_pose/src/flyingros_pose/laser_algorithm_functions.cpp
@@ -0,0 +1,43 @@
+/*
+ * laser_algorithm_functions.cpp
+ *
+ * Functions used by the position algorithm based on laser projections.
+ *
+ * This file is a part of FlyingROS
+ *
+ * FlyingROS free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * FlyingROS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with FlyingROS.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Software created by Alexis Paques and Nabil Nehri for the UCL
+ * in a Drone-Based Additive Manufacturing of Architectural Structures
+ * project financed by the MIT Seed Fund
+ *
+ * Copyright (c) Alexis Paques 2016
+ *
+ */
+
+#include "flyingros_pose/laser_algorithm_functions.h"
+
+namespace flyingros_pose
+{
+
+  void Laser::updateOrientation(tf::Vector3 _orientation){
+
+  }
+
+  double Laser::projectLaser(double _measure, tf::Quaternion _q){
+
+  }
+
+
+}
